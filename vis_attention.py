15fa8b87658ecfd33c1af549f49a9ad7f9ba8000
sdGu <48752972+Tarseus@users.noreply.github.com>
2024-06-28 18:48:41 +0800

    Delete vis_attention.py

--- vis_attention.py
+++
@@ -1,6 +0,0 @@
-import torch
-import matplotlib.pyplot as plt
-
-if __name__ == '__main__':
-    model = torch.load('vit_small_patch16_224_cifar100_pretrain.pth', map_location='cpu')
-    attention_weights = model.attention.qkv.weight.data
